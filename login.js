ef78e8296eb83ebd5034ebc9f461e7a9dffed676
MiguelCarlosRojas <miguel.carlos@vallegrande.edu.pe>
2022-12-14 12:56:51 -0500

    Initial commit

--- login.js
+++ login.js
@@ -30,12 +30,12 @@ app.get('/', function(request, response) {
 // http://localhost:3000/auth
 app.post('/auth', function(request, response) {
 	// Capture the input fields
-	let username = request.body.username;
-	let password = request.body.password;
+	let ingresoUsuario = request.body.username;
+	let ingresoContrasenia = request.body.password;
 	// Ensure the input fields exists and are not empty
 	if (username && password) {
 		// Execute SQL query that'll select the account from the database based on the specified username and password
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [ingresoUsuario, ingresoContrasenia], function(error, results, fields) {
 			// If there is an issue with the query, output the error
 			if (error) throw error;
 			// If the account exists
